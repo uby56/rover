53c24376c6d2e10bc93fcc12e694220594473916
uby56 <deorinabajyoti642@gmai.com>
2023-08-11 17:16:04 +0530

    Ros2 Control test

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -37,5 +37,18 @@ def generate_launch_description():
                                    '-entity', 'my_bot'],
                         output='screen')
 
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
+
+
     # Launch all the scripts
-    return LaunchDescription([rsp, gazebo, spawn_entity,])
+    return LaunchDescription([rsp, gazebo, spawn_entity,diff_drive_spawner,joint_broad_spawner])
