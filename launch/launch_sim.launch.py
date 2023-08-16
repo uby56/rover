838205fff05ff3c842609205c44377b60f2adfed
uby56 <deorinabajyoti642@gmai.com>
2023-08-16 16:52:42 +0530

    rover test file

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -4,8 +4,11 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessStart
 
 from launch_ros.actions import Node
 
@@ -25,8 +28,13 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+
     gazebo_params_path = os.path.join(
                   get_package_share_directory(package_name),'config','gazebo_params.yaml')
+    
+    # Directly below the controller manager node
+    delayed_controller_manager = TimerAction(period=3.0,actions=[controller_manager])
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     # gazebo = IncludeLaunchDescription(
@@ -35,16 +43,29 @@ def generate_launch_description():
     #             launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_path }.items()
     #      )
 
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+    # gazebo = IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource([os.path.join(
+    #                 get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+    #          )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
-                        output='screen')
+    # # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    # spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+    #                     arguments=['-topic', 'robot_description',
+    #                                '-entity', 'my_bot'],
+    #                     output='screen')
+
+    controller_params = os.path.join(
+        get_package_share_directory('articubot_one'), # <-- Replace with your package name
+        'config',
+        'my_controllers.yaml'
+        )
+
+    controller_manager = Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        parameters=[{'robot_description': robot_description},
+                controller_params],
+        )
 
     diff_drive_spawner = Node(
         package="controller_manager",
@@ -52,12 +73,30 @@ def generate_launch_description():
         arguments=["diff_cont"],
     )
 
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[diff_drive_spawner],
+        )
+    )
+
     joint_broad_spawner = Node(
         package="controller_manager",
         executable="spawner.py",
         arguments=["joint_broad"],
     )
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner],
+        )
+    )
 
 
     # Launch all the scripts
-    return LaunchDescription([rsp, gazebo, spawn_entity,diff_drive_spawner,joint_broad_spawner])
+    return LaunchDescription([
+    rsp,
+    delayed_controller_manager,
+    delayed_diff_drive_spawner,
+    delayed_joint_broad_spawner
+])
